a56dd292d9f051c5b20cdc0ffb5a039f6ae04b6d
bahrambds <bahrambds@gmail.com>
2019-11-06 22:55:40 -0500

    second commit

---
+++ content.js
@@ -0,0 +1,71 @@
+alert('THIS IS NOT WORKING')
+console.log('worskdsfsd')
+console.log('fourth time')
+// const intro = document.querySelector('#firstHeading')
+// let intro = document.getElementById('firstHeading');
+let body = document.body
+body.innerHTML = body.innerHTML.replace(/a/g, '<mark>a</mark>')
+// intro.textContent = 'YerrRESERESRRRRR'
+// intro.innerHTML= '<mark>i wokr?</mark>'
+// intro.setAttribute('style', 'background-color: yellow')
+
+// let words = ["Meet", "Odin!", "On", "several", "of", "our", "recent", "whale", "watching", "and", "wildlife", "tours,", "including", "our", "latest", "all-day", "tour,", "our", "passengers", "and", "crew", "have", "been", "lucky", "enough", "to", "see", "a", "new", "visitor"]
+
+// let innerHTML = document.body.innerHTML
+// let regex b
+
+// document.body.innerHTML = document.body.innerHTML.replace(/Highlighting/g, 'HELLOOOOOOOOO')
+
+
+// let child = document.body.getElementsByTagName('p')
+// console.log(child)
+// let childArray = child.innerText.split(" ");
+
+// let temp
+
+// for (let i = 0; i < childArray.length; i++) { 
+// temp = childArray[i];
+// childArray[i] = `<mark>${childArray[i]}</mark>`; 
+// child.innerHTML = childArray.join(" "); 
+// childArray[i] = temp;
+// child.innerHTML = childArray.join(" ");
+// }
+
+// let child = document.body.getElementsByTagName('p')[0]
+// let childArray = child.innerText.split(" ");
+// let temp
+
+// for (let i = 0; i < childArray.length; i++) { 
+// temp = childArray[0];s
+// childArray[0] = `<mark>${childArray[0]}</mark>`; 
+// child.innerHTML = childArray.join(" "); 
+// childArray[0] = temp;
+// child.innerHTML = childArray.join(" ");
+// }
+
+
+
+
+// console.log('HELLLOOOOOOO')
+
+// let inner = document.body.innerHTML
+// for (let i=0; i < 10; i++){
+// document.body.innerHTML = inner.replace(/Highlighting/g, `LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL`)
+// }
+// for (let ele of words) {
+//     regex = new RegExp("ReGex" + ele + "ReGex")
+//     console.log(ele)
+    // setTimeout(()=> console.log('wasting time'), 2000)
+
+// }
+// console.log(document.body.innerHTML)
+
+// let htmlText = ''
+
+// for (let key in pElements) htmlText += pElements[key].innerText
+
+
+// for (let ele of innerHTML.split(' ')) {
+//     let regex = new RegExp("ReGex" + ele + "ReGex")
+//     setTimeout((ele) => document.body.innerHTML = htmlText.replace(regex, `<mark>${ele}</mark>`), 1000)
+// }
